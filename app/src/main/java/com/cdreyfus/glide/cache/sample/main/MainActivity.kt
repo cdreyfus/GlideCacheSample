e456168a8f51360fa1a124cb97bfd85b7fe0baa7
cdreyfus <cecile.dreyfus@mwm.io>
2023-03-17 15:26:25 +0100

    Update deprecated image urls

--- app/src/main/java/com/cdreyfus/glide/cache/sample/main/MainActivity.kt
+++ app/src/main/java/com/cdreyfus/glide/cache/sample/main/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 "https://images.frandroid.com/wp-content/uploads/2019/08/android-eclair-1200x800.jpg"
             ),
             AndroidLogo(
-                "2.2", "https://hipertextual.com/archivo/wp-content/uploads/2010/05/froyo1.png"
+                "2.2", "https://i.blogs.es/c181f0/froyo/1366_2000.webp"
             ),
             AndroidLogo(
                 "2.3",
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             ),
             AndroidLogo(
                 "3.0",
-                "https://cnet3.cbsistatic.com/img/5o5uIVDPi0ydP-iffipMqwRybBQ=/470x353/2011/01/28/236e020d-cbf2-11e2-9a4a-0291187b029a/honeycomb-logo.jpg"
+                "https://upload.wikimedia.org/wikipedia/commons/9/9b/Android_Honeycomb_Logo.png"
             ),
             AndroidLogo(
                 "4.0",
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 "7.0", "https://images.frandroid.com/wp-content/uploads/2017/06/android-nougat.jpg"
             ),
             AndroidLogo(
-                "8.0,",
+                "8.0",
                 "https://www.sony.com/articleimage/servlet/servlet.FileDownload?file=0155F000007AvSLQA0"
             ),
             AndroidLogo(
